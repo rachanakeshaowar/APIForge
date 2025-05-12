1b491242e5158aed96a4f4eca88bbf2826dae672
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-12 18:08:45 +0530

    implement restricto  in controller

--- controller/authController.js
+++ controller/authController.js
@@ -122,4 +122,19 @@ const authentication = catchAsync(async (req, res, next) => {
     req.user = freshUser;
     return next();
 });
+const restrictTo = (...userType) => {
+    const checkPermission = (req, res, next) => {
+        if (!userType.includes(req.user.userType)) {
+            return next(
+                new AppError(
+                    "You don't have permission to perform this action",
+                    403
+                )
+            );
+        }
+        return next();
+    };
+
+    return checkPermission;
+};
 module.exports = { signup, login };
