b1884a68fbcf1cbd80792b0ade0d65c172a877a5
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-12 18:37:45 +0530

    update migrations

--- db/migrations/20250405111504-create-project.js
+++ db/migrations/20250405111504-create-project.js
@@ -1,45 +1,49 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('projects', {
+    await queryInterface.createTable("projects", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       isFeatured: {
-                type: Sequelize.BOOLEAN,
-                defaultValue: false,
-                allowNull: false,
-            },
-       price: {
-                type: Sequelize.DECIMAL,
-                allowNull: false,
-            },
-             shortDescription: {
-                type: Sequelize.TEXT,
-                allowNull: false,
-            },
-             description: {
-                type: Sequelize.TEXT,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      shortDescription: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      productUrl: {
+                type: Sequelize.STRING,
                 allowNull: false,
             },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('projects');
-  }
+    await queryInterface.dropTable("projects");
+  },
 };
