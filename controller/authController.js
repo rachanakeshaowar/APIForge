7ac61fbf0cc28a4836a4fa8817105453fe030e58
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-12 18:07:51 +0530

    implement authentication in controller

--- controller/authController.js
+++ controller/authController.js
@@ -98,5 +98,28 @@ const login = catchAsync(async (req, res, next) => {
         token,
     });
 });
+const authentication = catchAsync(async (req, res, next) => {
+    // 1. get the token from headers
+    let idToken = '';
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        // Bearer asfdasdfhjasdflkkasdf
+        idToken = req.headers.authorization.split(' ')[1];
+    }
+    if (!idToken) {
+        return next(new AppError('Please login to get access', 401));
+    }
+    // 2. token verification
+    const tokenDetail = jwt.verify(idToken, process.env.JWT_SECRET_KEY);
+    // 3. get the user detail from db and add to req object
+    const freshUser = await user.findByPk(tokenDetail.id);
 
+    if (!freshUser) {
+        return next(new AppError('User no longer exists', 400));
+    }
+    req.user = freshUser;
+    return next();
+});
 module.exports = { signup, login };
