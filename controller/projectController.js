cd8a98f43812bb181fcbe70e42f4f1568516f8de
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-12 18:12:46 +0530

    implement createProject in controller

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,25 +1,25 @@
 const project = require("../db/models/project");
 const catchAsync = require("../utils/catchAsync");
 
-const createProject = catchAsync(async (req, res, next) =>{
-  const body = req.body;
+const createProject = catchAsync(async (req, res, next) => {
+    const body = req.body;
+    const userId = req.user.id;
+    const newProject = await project.create({
+        title: body.title,
+        productImage: body.productImage,
+        price: body.price,
+        shortDescription: body.shortDescription,
+        description: body.description,
+        productUrl: body.productUrl,
+        category: body.category,
+        tags: body.tags,
+        createdBy: userId,
+    });
 
-  const newProject = await project.create ({
-    title: body.title,
-    productImage: body.productImage,
-    price: body.price,
-    shortDescription: body.shortDescription,
-    description: body.description,
-    productUrl: body.productUrl,
-    category: body.category,
-    tags: body.tags,
-    createdBy: 1,
-  });
-
-  return res.status(201).json({
-    status: 'success',
-    data: newProject,
-  })
+    return res.status(201).json({
+        status: 'success',
+        data: newProject,
+    });
 });
 
 module.exports = {createProject};
