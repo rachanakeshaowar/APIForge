f2806261a21b20fc685136314f74122417c5e1ef
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-13 13:47:33 +0530

    update route

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,4 +1,8 @@
-const { createProject } = require('../controller/projectController');
+const { authentication, restrictTo } = require('../controller/authController');
+const { createProject   getAllProject,
+    getProjectById,
+    updateProject,
+    deleteProject, } = require('../controller/projectController');
 
 const router = require('express').Router();
 
