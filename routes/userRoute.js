66a833afff02dbea9ac254e0b41f570532c2ab2d
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-13 13:49:56 +0530

    created user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,3 +3,6 @@ const { getAllUser } = require('../controller/userController');
 
 const router = require('express').Router();
 
+router.route('/').get(authentication, restrictTo('0'), getAllUser);
+
+module.exports = router;
